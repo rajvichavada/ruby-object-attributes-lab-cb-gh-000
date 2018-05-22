fd8a4213deac724146042382a1b689aebb424b28
Rajvi Chavada <rajvic@my.yorku.ca>
2018-05-22 23:16:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,24 @@
 class Dog
+  def intilize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 
-def initialize(name)
-  @name = name
-end
+  def name=(dog_name)
+    @name = dog_name
+  end
 
-def name=(dog_name)
-  @name = dogs_name
-end
+  def initial(breed)
+    @breed = breed
+  end
 
-def name
-  @name
-end
-
-def breed=(breed_name)
-  @breed = breed_name
-end
-
-def initialize(breed_name)
-  @breed = breed_name
-end
-
-def breed
-  @breed
-end
+  def breed
+    @breed
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
 
 end
